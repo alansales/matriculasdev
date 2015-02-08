0252d853bb3c4d702ec8c2343f13ec694f9db2c6
alansales <arquivosmorto@hotmail.com>
2015-02-08 13:44:05 -0200

    Matricula - Inclusão   objeto consultarData   teste exemplo

---
+++ teste.php
@@ -0,0 +1,33 @@
+<?php
+
+require_once('config/conn.php');
+
+// Instanciando novo objeto da classe Login
+$objLogin = new Login();
+
+// Verificando se o usuário está logado, caso contrá será redirecionado para a página de login
+if (!$objLogin->verificar('index.html')){
+    // Finalizado o script, apenas para garantir que o usuário irá ver o conteúdo da página
+    exit;   
+}
+ 
+//requerer objeto consultaData
+requeire_once('consutaData.php');
+$c = new consultarData();
+
+$mes='02';
+
+print 'Consultar matriculas do Mês de Fevereiro';
+$dados = mysql_query($c->porMes($mes));
+
+//listar das matriculas de fevereiro
+print 'Data         | QTDE';
+while($linha = mysql_fetch_assoc($dados)); 
+     print $linha['data'].'   '.$linha['quantidade'];
+} 
+
+print 'Retornar em string o mes selecionado';
+$data = mysql_query($c->retornarData($mes));
+print $c->strMes($data);
+    
+?>
